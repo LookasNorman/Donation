257a313781331be625e7751aa02afb6fcc9fdd5b
LookasNorman <lookasziebice@gmail.com>
2019-09-02 20:24:18 +0200

    Added institution list to home page

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Institution;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class HomeController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $institutions = $em->getRepository(Institution::class)->findAll();
+
         return $this->render('@App/home/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'institutions' => $institutions,
         ]);
     }
 }
