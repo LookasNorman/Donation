1271146ec8e8bc714b9c60ab8f7968eb7ee7d1ef
LookasNorman <lookasziebice@gmail.com>
2019-09-05 12:15:32 +0200

    Added username to user menu

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,9 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class UserController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loggedUserMenuAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+        $user->getId();
+
+        $loggedUser = $em->getRepository(User::class)->find($user);
+
+        return $this->render('@App/menu/logged-user.html.twig', [
+            'loggedUser' => $loggedUser,
+        ]);
+    }
 }
