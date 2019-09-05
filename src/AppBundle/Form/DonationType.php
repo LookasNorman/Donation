223b9a9aca0f021c7cc35d33ff85885ee63c15bb
LookasNorman <lookasziebice@gmail.com>
2019-09-05 14:04:30 +0200

    Formated form without address and institution

--- src/AppBundle/Form/DonationType.php
+++ src/AppBundle/Form/DonationType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -30,7 +31,7 @@ class DonationType extends AbstractType
                 'attr' => [
                     'name' => 'categories',
                     'class' => 'checkbox',
-                ]
+                ],
             ])
             ->add('institution', EntityType::class, [
                 'class' => Institution::class,
@@ -46,7 +47,8 @@ class DonationType extends AbstractType
                 'attr' => [
                     'step' => 1,
                     'min' => 1,
-                ]
+                ],
+                'label' => false,
             ])
             ->add('street', TextType::class, [
                 'required' => true,
@@ -65,7 +67,7 @@ class DonationType extends AbstractType
                 'widget' => 'single_text',
                 'required' => true,
             ])
-            ->add('pickUpComment', TextType::class, [
+            ->add('pickUpComment', TextareaType::class, [
                 'required' => true,
             ])
             ->add('phone', TextType::class, [
