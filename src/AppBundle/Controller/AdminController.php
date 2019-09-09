abc91b791232125e411c869ca83d92181ab88c9a
LookasNorman <lookasziebice@gmail.com>
2019-09-09 21:59:55 +0200

    Added new admin - promote user to admin

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class AdminController
  * @package AppBundle\Controller
  * @Route("admin")
+ * @Security("is_granted('ROLE_ADMIN')")
  */
 class AdminController extends Controller
 {
@@ -28,6 +30,25 @@ class AdminController extends Controller
         return $this->render('@App/user/index.html.twig', array(
             'users' => $users,
         ));
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/promote/{id}", name="user_promote", methods={"GET"})
+     */
+    public function promoteUser($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->find($id);
+
+        $user->addRole("ROLE_ADMIN");
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirectToRoute('user_show', [
+            'id' => $id,
+        ]);
     }
 
 }
