2a5c482b4f75d6560afaac55964b21c3b11e5334
LookasNorman <lookasziebice@gmail.com>
2019-09-15 18:39:23 +0200

    Added pasword strenght meter

--- web/assets/js/form.js
+++ web/assets/js/form.js
@@ -16,6 +16,39 @@ $(function () {
             $('#time-summary').text($('input[name="pickUpTime"]').val());
             $('#comments-summary').text($('textarea[name="pickUpComment"]').val());
 
-    })
+    });
 
+    jQuery.strength = function( element, password ) {
+        var desc = [{'width':'0px'}, {'width':'20%'}, {'width':'40%'}, {'width':'60%'}, {'width':'80%'}, {'width':'100%'}];
+        var descClass = ['', 'progress-bar-danger', 'progress-bar-danger', 'progress-bar-warning', 'progress-bar-success', 'progress-bar-success'];
+        var score = 0;
+
+        if(password.length > 6){
+            score++;
+        }
+
+        if ((password.match(/[a-z]/)) && (password.match(/[A-Z]/))){
+            score++;
+        }
+
+        if(password.match(/\d+/)){
+            score++;
+        }
+
+        if(password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/)){
+            score++;
+        }
+
+        if (password.length > 10){
+            score++;
+        }
+
+        element.removeClass( descClass[score-1] ).addClass( descClass[score] ).css( desc[score] );
+    };
+
+    jQuery(function() {
+        jQuery("#fos_user_registration_form_plainPassword_first").keyup(function() {
+            jQuery.strength(jQuery("#progress-bar"), jQuery(this).val());
+        });
+    });
 })
